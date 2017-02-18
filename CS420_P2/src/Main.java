6341ba761492e8a72d96f9512d3cd79cfab8ef23
Gabriel <gortegagingrich@gmail.com>
2017-02-17 16:10:15 -0800

    initial commit

---
+++ CS420_P2/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Gabriel on 2017/02/17.
+ */
+public class Main {
+   public static void main(String[] args) {
+      System.out.println("This doesn't do anything yet");
+   }
+
+}
